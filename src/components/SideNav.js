9a9b155e0eb398b486875a78f3e1fea53b7d3e8d
ihopethisnamefi <jonthibeault@gmail.com>
2017-11-08 20:50:48 -0600

    First Commit

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,26 +1,50 @@
 import React from "react";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function SideNav() {
   return (
     <div className="collapse navbar-collapse navbar-ex1-collapse">
       <ul className="nav navbar-nav side-nav">
         <li className="active">
-          {/*
           <Link to="/"> <i className="fa fa-fw fa-dashboard" />
             Dashboard 
           </Link>
-          */}
+
         </li>
         <li>
-          <a href="charts.html">
+          <Link to="/charts">
             <i className="fa fa-fw fa-bar-chart-o" /> Charts
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="tables.html">
+          <Link to="/tables">
             <i className="fa fa-fw fa-table" /> Tables
-          </a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/settings">
+            <i className="fa fa-fw fa-bar-chart-o" /> Settings
+          </Link>
+        </li>
+        <li>
+          <Link to="/wall">
+            <i className="fa fa-fw fa-table" /> Wall
+          </Link>
+        </li>
+        <li>
+          <Link to="/profiles">
+            <i className="fa fa-fw fa-bar-chart-o" /> Profiles
+          </Link>
+        </li>
+        <li>
+          <Link to="/marquee/iloveroutes">
+            <i className="fa fa-fw fa-table" /> Marquee I love routes
+          </Link>
+        </li>
+        <li>
+          <Link to="/marquee/reactroutesrule">
+            <i className="fa fa-fw fa-table" /> Marquee Routes Rule
+          </Link>
         </li>
       </ul>
     </div>);  
